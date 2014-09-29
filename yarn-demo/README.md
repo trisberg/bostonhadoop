0e7faa1ccee1dc31c1515ba3d81a5078b1bbadba
Thomas Risberg <trisberg@vmware.com>
2014-09-29 15:10:40 -0400

    Adding simple YARN demo app

---
+++ yarn-demo/README.md
@@ -0,0 +1,7 @@
+Simple YARN app
+===============
+
+Build and run using:
+
+  gradle clean build
+  java -jar build/libs/yarn-demo-0.1.0.jar
---
+++ yarn-demo/src/main/java/com/springdeveloper/demo/yarn/Application.java
@@ -0,0 +1,33 @@
+package com.springdeveloper.demo.yarn;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.yarn.annotation.OnContainerStart;
+import org.springframework.yarn.annotation.YarnComponent;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+    	new HelloPojo();
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @YarnComponent
+    @Profile("container")
+    public static class HelloPojo {
+    	private static final Log log = LogFactory.getLog(HelloPojo.class);
+    	
+    	@OnContainerStart
+    	public void onStart() {
+    		log.info("Hello from YARN!");
+    	}
+	}
+}
